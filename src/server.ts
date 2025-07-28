c60528cdee201d2366cf5a97abb01d617a8b5926
Biswajit Das <djdas000000@gmail.com>
2025-07-28 17:15:10 +0530

    add home url

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ export function buildServer() {
   const app = Fastify({ logger: true });
 
   app.register(cors);
+  // Home Route
+app.get('/', async (req, res) => {
+res.type('text/html').send('<h1>✅ Portify Backend is Running</h1>')
+})
   app.register(projectRoutes, { prefix: '/api/projects' });
 
   return app;
